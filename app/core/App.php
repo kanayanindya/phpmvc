4db75b31664c046b0f3a7fe677a4abf15e6c7b17
kanayanindya <wintorogaulz@gmail.com>
2021-12-08 13:58:32 +0700

    file akhir video 3-routing tapi masih gagal menangkap array halaman

--- app/core/App.php
+++ app/core/App.php
@@ -3,6 +3,17 @@
 class App{
     public function __construct()
     {
-        echo 'OK!';
+        $url = $this->parseURL();
+        var_dump($url);
+    }
+
+    public function parseURL()
+    {
+        if(isset($_GET['url'])){
+            $url =  rtrim($_GET['url'], '/'); // menghapus tanda / pada url
+            $url = filter_var($url, FILTER_SANITIZE_URL); // membersihkan url dari karakter2 aneh
+            $url = explode('/', $url);
+            return $url;
+        }
     }
 }
